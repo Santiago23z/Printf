b1daf530579ee3ee3a211e708f870c23153902b2
santiago23z <santizapataalvares@gmail.com>
2022-03-23 15:39:01 +0000

    Main

--- main.h
+++ main.h
@@ -2,6 +2,9 @@
 #define _MAIN_H
 
 #include <stdarg.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 /**
  * Structure - Structure for printing variuos
@@ -21,14 +24,5 @@ int print_c(va_list c);
 int print_s(va_list s);
 int print_i(va_list i);
 int print_d(va_list d);
-int print_u(va_list u);
-int print_b(va_list b);
-int print_o(va_list o);
-int print_x(va_list x);
-int print_X(va_list X);
-int print_p(va_list p);
-int print_S(va_list S);
-int print_r(va_list r);
-int print_R(va_list R);
 
 #endif /* _MAIN_H */ 
