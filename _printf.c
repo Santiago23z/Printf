24a8a2ce07b9c7e28858383a68c798efe70973ac
santiago23z <santizapataalvares@gmail.com>
2022-03-24 14:58:42 +0000

    s

--- _printf.c
+++ _printf.c
@@ -9,41 +9,40 @@
  */
 int _printf(const char *format, ...)
 {
-	unsigned int i = 0, count = 0;
-	va_list valist;
-	int (*f)(va_list);
-	
-	va_start(valist, format);
-	if (!format || (format[0] == '%' && !format[1]))
-        	return (-1);
-    	if (format[0] == '%' && format[1] == ' ' && !format[2])
-        	return (-1);
+unsigned int i = 0, count = 0;
+va_list valist;
+int (*f)(va_list);
+va_start(valist, format);
+if (!format || (format[0] == '%' && !format[1]))
+return (-1);
+if (format[0] == '%' && format[1] == ' ' && !format[2])
+return (-1);
 
-	while (format[i] != '\0')
-	{
-		if (format[i] != '%')
-		{
-			_putchar(format[i]);
-			count++;
-		}
-		else
-		{
-			i++;
-			if (format[i] == '%')
-			{
-				count += _putchar('%');
-			}
-			else
-			{
-				f = get_function(format[i]);
-				if (!f)
-					_printf("%%%c", format[i]);
-				else
-					count += f(valist);
-			}
-		}
-		i++;
-	}	
-	va_end(valist);
-	return (count);
+while (format[i] != '\0')
+{
+if (format[i] != '%')
+{
+_putchar(format[i]);
+count++;
+}
+else
+{
+i++;
+if (format[i] == '%')
+{
+count += _putchar('%');
+}
+else
+{
+f = get_function(format[i]);
+if (!f)
+_printf("%%%c", format[i]);
+else
+count += f(valist);
+}
+}
+i++;
+}
+va_end(valist);
+return (count);
 }
